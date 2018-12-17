1a63de82668ce50f1bd5d34bceae31fb10d8c2dc
Marcelo Fabian <marcelo.docutec@gmail.com>
2018-12-17 00:29:21 -0300

    Se agrego login y logout con reconocimiento de datos a la app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      user: null
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleAuth(){
+    //esto crea un proveedor de google para utilizar
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    //este sigIn devuelve una promesa
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            console.log(`${result.user.displayName} ha inciado sesion`)
+        })
+        .catch(error => {
+          console.log(`Error ${error.code}: ${error.message}`)
+        })
+  }
+
+  handleLogout(){
+    firebase.auth().signOut() //tambien devuelve una promesa
+        .then(result => {
+            console.log(`${result.user.displayName} ha salido del sistema`)
+        })
+        .catch(error => {
+            console.log(`Error ${error.code}: ${error.message}`)
+        })
+  }
+
+  renderLoginButton(){
+    if( this.state.user ){
+      return (
+          <div>
+              <img className="avatar" src={this.state.user.photoURL} alt={this.state.user.displayName}/>
+              <p>Bienvenido {this.state.user.displayName} !!</p>
+              <button onClick={this.handleLogout}>Salir</button>
+          </div>
+      );
+    }else {
+        return(
+            <button className="" onClick={this.handleAuth}>Sign in with Google</button>
+        );
+    }
+  }
+  //es un metodo de ciclo de vida, que se dispara una vez que haya sido renderizado en DOM
+  componentWillMount(){
+    firebase.auth()
+        .onAuthStateChanged(user =>{
+          this.setState({
+              user: user //ahora user vale lo que nos devuelve la funcion onAuthState
+          })
+        })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h2>Not Instagram App</h2>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+              {this.renderLoginButton()}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAe6S_aCnbNMBXyUQao2MvXc3jI5L_dsjc",
+    authDomain: "not-instagram-bf013.firebaseapp.com",
+    databaseURL: "https://not-instagram-bf013.firebaseio.com",
+    projectId: "not-instagram-bf013",
+    storageBucket: "not-instagram-bf013.appspot.com",
+    messagingSenderId: "825893006884"
+});
+
+ReactDOM.render(
+    <App />, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
+
+
+
